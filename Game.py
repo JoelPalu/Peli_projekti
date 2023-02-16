dd5896f3838d68b5d72e51c2b53c2c8382a02143
JoelPalu <122279670+JoelPalu@users.noreply.github.com>
2023-02-16 14:08:10 +0200

    Create new user

--- Game.py
+++ Game.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from time import sleep
 import os
+
 import geopy.distance
 
 connection = mysql.connector.connect(
@@ -24,14 +25,26 @@ def search_ICAO(input1, input2):
     return airport
 
 
+def create_new_player(username):
+    sql_command = "INSERT INTO player_stat(screen_name, fuel_consumed, fuel, points) values('" + username + "', 0, 10000,0)"
+    cursor = connection.cursor()
+    cursor.execute(sql_command)
+    total = cursor.fetchall()
+    if cursor.rowcount > 0:
+        for row in total:
+            print(row)
 
 
-ICAO1,ICAO2= input("Anna ekan kentän ISCO: "), input("Anna tokan kentän ISCO: ")
+
+
+player_name = input("Name the player: ")
+create_new_player(player_name)
+"""ICAO1,ICAO2= input("Anna ekan kentän ISCO: "), input("Anna tokan kentän ISCO: ")
 cordinates = search_ICAO(ICAO1,ICAO2)
 place1 = cordinates[0],cordinates[1]
 place2 = cordinates[2], cordinates[3]
 print(f"Lentokenttien etäisyys on: {geopy.distance.distance(place1,place2).km:1.2f}km")
-
+"""
 
 sleep(4) #Peli jäätyy näkymään ja ei ota inputteja vastaan
 os.system('cls') # puhdistaa taulun, niin voi piirtää uuden taulun.
