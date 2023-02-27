05e7b7038358a65f09baa992c49b04e92477c0fa
JoelPalu <122279670+JoelPalu@users.noreply.github.com>
2023-02-27 14:08:17 +0200

    27.2  updates

--- Game/Menu.py
+++ Game/Menu.py
@@ -1,7 +1,10 @@
-def print_username(user):
-    print(user)
+import os
+
+#Peli prosessin ylä osa UI:sta
 def head_UI(user,money):
     print(f"________________________________________________________")
     print(f"{user}            {money}")
+
+#UI:n ala ja yläviiva
 def bot_UI():
     print(f"________________________________________________________")
